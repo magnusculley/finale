fb26d5cdbbcd0d7a27d6e8072fa6f4e4019ef79d
magnus <magnus@udel.edu>
2023-11-03 00:18:38 -0400

    fixed bug but also changing so much

--- final.py
+++ final.py
@@ -2,43 +2,71 @@ from designer import *
 from dataclasses import dataclass
 
 @dataclass
+class Stage:
+    blocks:[DesignerObject]
+
+#@dataclass
+#class Clicker:
+
+@dataclass
 class Player:
     obj: DesignerObject
     is_moving: bool
+    colliding_with_block: bool
     xspeed: int
     yspeed: float
     jump_delay:int
+    beam :DesignerObject
+
 
 @dataclass
 class World:
     player: Player
     timer:int
     gravity: int
+    stages: [Stage]
 
 pressed_keys = []
 FIRST_JUMP =True
 
 def create_world() -> World:
-    return World(create_player(), 0, 1)
+    return World(create_player(), 0, 1,create_stages())
 
 def advance_the_timer(world: World):
     world.timer += 1
 
+def create_stages():
+    return [Stage([rectangle('black', get_width(),80, get_width()/2, get_height()-40),rectangle('black',get_width(),80, get_width()/2, 40)])]
+
 def create_player() -> Player:
-    return Player(emoji("man"), False, 3, 0.0,180)
+    return Player(emoji("🔴"), False, False, 3, 0.0,0,[])
 
 def physics(world: World):
-    world.player.yspeed += world.gravity
+    if colliding(world.player.obj, world.stages[0].blocks[0]):
+        if not world.player.colliding_with_block:
+            world.player.yspeed = 0
+            world.player.colliding_with_block = True
+    else:
+        world.player.colliding_with_block = False
+        world.player.yspeed += world.gravity
+
+def clicked(world: World):
+    world.player.beam=(line('blue', world.player.obj.x, world.player.obj.y, get_mouse_x(), get_mouse_y(),2))
+
+#def unclicked(world: World):
+def unclicked(world: World):
+    destroy(world.player.beam)
+
 
 def key_pressed(world: World, key: str):
     global FIRST_JUMP
     pressed_keys.append(key)
     if 'left' in pressed_keys and 'right' in pressed_keys:
         world.player.xspeed = 0
-    elif 'left' in pressed_keys:
+    if 'left' in pressed_keys:
         world.player.is_moving = True
         world.player.xspeed = -10
-    elif 'right' in pressed_keys:
+    if 'right' in pressed_keys:
         world.player.is_moving = True
         world.player.xspeed = 10
     if key == 'space':
@@ -64,7 +92,7 @@ def jump(world: World):
     if FIRST_JUMP:
         world.player.yspeed = -20
         world.player.jump_delay=time
-    elif time-world.player.jump_delay >38:
+    elif time-world.player.jump_delay >10 and world.player.colliding_with_block:
         print(time-world.player.jump_delay,"JUMPED")
         world.player.yspeed = -20
         world.player.jump_delay = time
@@ -76,10 +104,8 @@ def player_movement(world: World):
 
     world.player.obj.y += world.player.yspeed
 
-    if world.player.obj.y > 200:
-        world.player.obj.y = 200
-        world.player.yspeed = 0
-
+when('input.mouse.down', clicked)
+when('input.mouse.up', unclicked)
 when('updating', physics)
 when('updating', player_movement)
 when('typing', key_pressed)
